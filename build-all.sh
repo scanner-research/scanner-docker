dd3b44207e4d80e17b91ae1c9dfade184ecef71e
Will Crichton <wcrichto@stanford.edu>
2017-01-25 12:21:58 -0500

    Added gRPC

--- build-all.sh
+++ build-all.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+NO_CACHE=false
+
 for dir in ./*/
 do
     dir=`basename ${dir%*/}`
     tag=scannerresearch/scanner-base:$dir
-    docker build -t $tag $dir
+    docker build --no-cache=$NO_CACHE -t $tag $dir
     docker push $tag
 done
