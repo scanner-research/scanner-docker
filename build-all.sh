97c8a94eb357856414b733e12b9c9ff230cb50bf
Will Crichton <wcrichto@stanford.edu>
2017-02-02 02:51:52 -0500

    Added parallel build

--- build-all.sh
+++ build-all.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 NO_CACHE=false
+CORES=16
 
 for dir in ./*/
 do
     dir=`basename ${dir%*/}`
     tag=scannerresearch/scanner-base:$dir
-    docker build --no-cache=$NO_CACHE -t $tag $dir
+    docker build --build-arg cores=$CORES --no-cache=$NO_CACHE -t $tag $dir
     docker push $tag
 done
