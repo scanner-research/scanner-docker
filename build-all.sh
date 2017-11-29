5877f8f579fa4e7ece607d5526000aa09d513915
Alex Poms <alexpoms@gmail.com>
2017-11-29 00:24:59 -0800

    Add CPU only image for Caffe

--- build-all.sh
+++ build-all.sh
@@ -6,7 +6,14 @@ CORES=16
 for dir in ./*/
 do
     dir=`basename ${dir%*/}`
-    tag=scannerresearch/scanner-base:$dir
-    docker build --build-arg cores=$CORES --no-cache=$NO_CACHE -t $tag $dir
-    docker push $tag
+
+    tag=scannerresearch/scanner-base:$dir-gpu
+    docker build --build-arg cores=$CORES \
+           --no-cache=$NO_CACHE -t $tag $dir
+    docker push $tag-gpu
+
+    tag=scannerresearch/scanner-base:$dir-cpu
+    docker build --build-arg cores=$CORES --build-arg cpu_only=ON \
+           --no-cache=$NO_CACHE -t $tag $dir
+    docker push $tag-cpu
 done
